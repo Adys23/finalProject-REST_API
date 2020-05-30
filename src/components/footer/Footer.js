1407faf4bcc8b0c7ae1c15dd1a943fe969b080a2
Adrian W <wasilewski.adrian.64@gmail.com>
2020-05-30 08:50:33 +0200

    footer text updated

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,6 +2,15 @@ import React from 'react';
 
 import classes from './Footer.module.scss';
 
-const footer = (props) => <div className={classes.Footer}>This is footer</div>;
+const footer = (props) => (
+	<div className={classes.Footer}>
+		<div>Live data downloaded from official FPL API.</div>
+		<div>
+			Have you found this site interesting? You have some questions or want to
+			leave your opinion? Use Contact tab.
+		</div>
+		<div className={classes.Copyrights}>Copyrights by Adrian Wasilewski</div>
+	</div>
+);
 
 export default footer;
