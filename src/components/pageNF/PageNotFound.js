d05f6593bafa2ccdd8974877a647587952ecaaad
Adrian W <wasilewski.adrian.64@gmail.com>
2020-06-02 15:49:28 +0200

    page not found added

--- src/components/pageNF/PageNotFound.js
+++ src/components/pageNF/PageNotFound.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
-const pageNF = (props) => <div>Page was not found</div>;
+import classes from './PageNotFound.module.scss';
+
+const pageNF = () => (
+	<>
+		<div className={classes.Container}>
+			<div className={classes.PageNF}> </div>
+			<a href='http://www.freepik.com'>Designed by pikisuperstar / Freepik</a>
+		</div>
+	</>
+);
 
 export default pageNF;
