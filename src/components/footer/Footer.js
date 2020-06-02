0b6e9819a78c35c15da1201fac3aa787c28df90d
Adrian W <wasilewski.adrian.64@gmail.com>
2020-06-02 13:19:42 +0200

    mainPage created

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -15,6 +15,16 @@ const footer = (props) => (
 			Have you found this site interesting? You have some questions or want to
 			leave your opinion? Use <a href='/contact'>Contact</a> tab.
 		</div>
+		<div>
+			Icons made by{' '}
+			<a href='https://www.flaticon.com/authors/freepik' title='Freepik'>
+				Freepik
+			</a>{' '}
+			from{' '}
+			<a href='https://www.flaticon.com/' title='Flaticon'>
+				www.flaticon.com
+			</a>
+		</div>
 		<div className={classes.Copyrights}>
 			Copyrights by Adrian Wasilewski &copy;
 		</div>
--- src/containers/main/Index.js
+++ src/containers/main/Index.js
@@ -1,5 +1,38 @@
 import React from 'react';
+import Aux from '../../hoc/Auxiliary';
 
-const index = (props) => (<div id="header-featured"> </div><div>This is main page</div>);
+import classes from './Index.module.scss';
+
+const index = (props) => (
+	<Aux>
+		<div className={classes.Container}>
+			<div className={classes.HeaderFeatured}> </div>
+			<div className={classes.FeaturedWrapper}>
+				<div className={classes.title}>This is FPL Data</div>
+				<div className={classes.column1}>
+					<span className={classes.icon1}></span>
+					<p>
+						Find up-to-date statistics of players and teams to give you the full
+						overview of their performace.
+					</p>
+				</div>
+				<div className={classes.column2}>
+					<span className={classes.icon2}></span>
+					<p>
+						Simplify your Fantasy Premier League choices! Make sure you do not
+						miss any bargain!
+					</p>
+				</div>
+				<div className={classes.column3}>
+					<span className={classes.icon3}></span>
+					<p>
+						Dive deep into the broadest variety of information that you need tyo
+						beat your opponents!
+					</p>
+				</div>
+			</div>
+		</div>
+	</Aux>
+);
 
 export default index;
