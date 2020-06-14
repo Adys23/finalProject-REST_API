acabe04bb4cd92c60094d4fe062790a45aa62995
Adrian W <wasilewski.adrian.64@gmail.com>
2020-06-14 08:56:11 +0200

    Updates

---
+++ precache-manifest.1e8d30a69f0f2cb005697d85cee3b47f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d5d274992199fa8f7f02ae84236fdb6",
+    "url": "/finalProject-REST_API/index.html"
+  },
+  {
+    "revision": "ad8e0fac0a4e6f5b26ef",
+    "url": "/finalProject-REST_API/static/css/main.1cc73f97.chunk.css"
+  },
+  {
+    "revision": "6d248abee25bb5f6554d",
+    "url": "/finalProject-REST_API/static/js/2.b04d0b19.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/finalProject-REST_API/static/js/2.b04d0b19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad8e0fac0a4e6f5b26ef",
+    "url": "/finalProject-REST_API/static/js/main.f67cb5e1.chunk.js"
+  },
+  {
+    "revision": "81972e13d8c92d5ee9b5",
+    "url": "/finalProject-REST_API/static/js/runtime-main.eb675ace.js"
+  },
+  {
+    "revision": "e072285821dfed53403038f7e0621a74",
+    "url": "/finalProject-REST_API/static/media/2704133.e0722858.jpg"
+  },
+  {
+    "revision": "aea70f4b660721fa36294bcf717e7be1",
+    "url": "/finalProject-REST_API/static/media/fpl-logo.aea70f4b.png"
+  },
+  {
+    "revision": "5fd24cc23a0ebe144ccc35b59468d2cf",
+    "url": "/finalProject-REST_API/static/media/stadium.5fd24cc2.jpg"
+  }
+]);
